a688c22cd33cfe447bbab1764c2e635d0e6f0af8
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-02-14 14:35:35 +1000

    🧱 Generate basic components

---
+++ src/app/component/chatroom/chatroom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatroom',
+  imports: [],
+  templateUrl: './chatroom.component.html',
+  styleUrl: './chatroom.component.scss'
+})
+export class ChatroomComponent {
+
+}
