f283006783cc60f49af791a72bcc267db4cc3e55
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-03-06 22:18:29 +1000

    💐 Add an animation to the bottom

--- src/app/component/chatroom/chatroom.component.ts
+++ src/app/component/chatroom/chatroom.component.ts
@@ -11,6 +11,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MistralApiService } from '../../services/mistral-api.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 export interface Message {
   message: string;
@@ -37,10 +45,27 @@ export interface Message {
   ],
   templateUrl: './chatroom.component.html',
   styleUrl: './chatroom.component.scss',
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          height: '500px'
+        }),
+      ),
+      state(
+        'closed',
+        style({
+          height: '0px'
+        }),
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
+  ])]
 })
 export class ChatroomComponent {
   public messages: Message[] = [];
-
+  public isOpen = false;
   public questionInput = new FormControl('');
 
   constructor(private mistral: MistralApiService) {}
@@ -56,6 +81,7 @@ export class ChatroomComponent {
     if(!this.questionInput.value){
       return;
     }
+    this.isOpen = true;
     const question = this.questionInput.value;
     this.questionInput = new FormControl('');
     this.messages.push({ author: 'user', message: question });
