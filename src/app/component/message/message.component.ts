75d1e99e96f8fe383f5881750935e7cfd870995f
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-03-08 21:21:43 +1000

    🧱 Add ngx-markdown and manage md format messages

--- src/app/component/message/message.component.ts
+++ src/app/component/message/message.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { Message } from '../chatroom/chatroom.component';
 import { CommonModule } from '@angular/common';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-message',
   imports: [MatCardModule, 
-      CommonModule],
+      CommonModule,
+    MarkdownModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
