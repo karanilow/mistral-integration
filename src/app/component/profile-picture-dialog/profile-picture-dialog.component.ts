bf4710f16c8b8851baaed2d7012f431f12474b15
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-03-10 21:36:34 +1000

    ⚒️ Add the Selected effect and property

--- src/app/component/profile-picture-dialog/profile-picture-dialog.component.ts
+++ src/app/component/profile-picture-dialog/profile-picture-dialog.component.ts
@@ -5,6 +5,11 @@ import {
   MatDialogContent,
 } from '@angular/material/dialog';
 
+interface ProfilePicture{
+  src: string;
+  isSelected: boolean;
+}
+
 @Component({
   selector: 'app-profile-picture-dialog',
   imports: [MatDialogTitle, MatDialogContent, CommonModule],
@@ -13,5 +18,19 @@ import {
 })
 export class ProfilePictureDialogComponent {
   public profilePictures = ["user-1.jpg", "user-2.jpg", "user-3.jpg", "user-4.jpg"] 
-  
+  public profilePictureList : ProfilePicture[] = [];
+  public selectedProfilePicture: ProfilePicture;
+
+  constructor() {
+    this.profilePictureList = this.profilePictures.map((src) => ({ src, isSelected: false }));
+    this.profilePictureList[0].isSelected = true;
+    this.selectedProfilePicture = this.profilePictureList[0];
+  }
+
+  public selectProfilePicture(profilePicture: ProfilePicture) {
+    this.selectedProfilePicture.isSelected = false;
+    profilePicture.isSelected = true;
+    this.selectedProfilePicture = profilePicture;
+  }
+
 }
